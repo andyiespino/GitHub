90cea8bf27d763ccdf8db35ee973c244c3ef91f5
andresespinoza <andresespinoza@192.168.65.132>
2022-11-17 11:44:40 -0700

    Prueba Número: 1

--- src/ejemplogithub/EjemploGitHub.java
+++ src/ejemplogithub/EjemploGitHub.java
@@ -14,6 +14,7 @@ public class EjemploGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println ("Hola");
         // TODO code application logic here
     }
     
